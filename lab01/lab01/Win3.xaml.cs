54e1d1d9ee27c6e87aeb48aa2c0e1387382b529b
Leonid <90851358+BickslowX@users.noreply.github.com>
2022-02-11 22:59:10 +0200

    Lab01 Done
    Lab01 Done

---
+++ lab01/lab01/Win3.xaml.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab01
+{
+    /// <summary>
+    /// Логика взаимодействия для Win3.xaml
+    /// </summary>
+    public partial class Win3 : Window
+    {
+        static  bool d = false;
+        public Win3()
+        {
+            InitializeComponent();
+        }
+        private void Back3_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            Hide();
+            mw.Show();
+        }
+        private void reload()
+        {
+            Win3 ww3 = new Win3();
+            Hide();
+            ww3.Show();
+        }
+        private void comp_move()
+        {
+            bool flag = false;
+            Random r = new Random();
+            while(flag == false)
+            {
+                int num = r.Next(0,25);
+                int counter = 0;
+                foreach (Button b in TicTac.Children.OfType<Button>())
+                {
+                    if (counter == num && (string)b.Content == "▢")
+                    {
+                        b.Content = "⭕";
+                        b.IsEnabled = false;
+                        flag = true;
+                        return;
+                    }
+                    counter++;
+                }
+            }
+            win_check();
+        }
+        private void win_check()
+        {
+            //
+            if(((string)f00.Content== "❎"&& (string)f01.Content == "❎" && (string)f02.Content == "❎" && (string)f03.Content == "❎")|| ((string)f01.Content == "❎" && (string)f02.Content == "❎" && (string)f03.Content == "❎" && (string)f04.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f10.Content == "❎" && (string)f11.Content == "❎" && (string)f12.Content == "❎" && (string)f13.Content == "❎") || ((string)f11.Content == "❎" && (string)f12.Content == "❎" && (string)f13.Content == "❎" && (string)f14.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f20.Content == "❎" && (string)f21.Content == "❎" && (string)f22.Content == "❎" && (string)f23.Content == "❎") || ((string)f21.Content == "❎" && (string)f22.Content == "❎" && (string)f23.Content == "❎" && (string)f24.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f30.Content == "❎" && (string)f31.Content == "❎" && (string)f32.Content == "❎" && (string)f33.Content == "❎") || ((string)f31.Content == "❎" && (string)f32.Content == "❎" && (string)f33.Content == "❎" && (string)f34.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f40.Content == "❎" && (string)f41.Content == "❎" && (string)f42.Content == "❎" && (string)f43.Content == "❎") || ((string)f41.Content == "❎" && (string)f42.Content == "❎" && (string)f43.Content == "❎" && (string)f44.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f00.Content == "❎" && (string)f10.Content == "❎" && (string)f20.Content == "❎" && (string)f30.Content == "❎") || ((string)f10.Content == "❎" && (string)f20.Content == "❎" && (string)f30.Content == "❎" && (string)f40.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f01.Content == "❎" && (string)f11.Content == "❎" && (string)f21.Content == "❎" && (string)f31.Content == "❎") || ((string)f11.Content == "❎" && (string)f21.Content == "❎" && (string)f31.Content == "❎" && (string)f41.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f02.Content == "❎" && (string)f12.Content == "❎" && (string)f22.Content == "❎" && (string)f32.Content == "❎") || ((string)f12.Content == "❎" && (string)f22.Content == "❎" && (string)f32.Content == "❎" && (string)f42.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f03.Content == "❎" && (string)f13.Content == "❎" && (string)f23.Content == "❎" && (string)f33.Content == "❎") || ((string)f13.Content == "❎" && (string)f23.Content == "❎" && (string)f33.Content == "❎" && (string)f43.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f04.Content == "❎" && (string)f14.Content == "❎" && (string)f24.Content == "❎" && (string)f34.Content == "❎") || ((string)f14.Content == "❎" && (string)f24.Content == "❎" && (string)f34.Content == "❎" && (string)f44.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f00.Content == "❎" && (string)f11.Content == "❎" && (string)f22.Content == "❎" && (string)f33.Content == "❎") || ((string)f11.Content == "❎" && (string)f22.Content == "❎" && (string)f33.Content == "❎" && (string)f44.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if (((string)f40.Content == "❎" && (string)f31.Content == "❎" && (string)f22.Content == "❎" && (string)f13.Content == "❎") || ((string)f31.Content == "❎" && (string)f22.Content == "❎" && (string)f13.Content == "❎" && (string)f04.Content == "❎"))
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if ((string)f01.Content == "❎" && (string)f12.Content == "❎" && (string)f23.Content == "❎" && (string)f34.Content == "❎")
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if ((string)f10.Content == "❎" && (string)f21.Content == "❎" && (string)f32.Content == "❎" && (string)f43.Content == "❎")
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if ((string)f03.Content == "❎" && (string)f12.Content == "❎" && (string)f21.Content == "❎" && (string)f30.Content == "❎")
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            if ((string)f14.Content == "❎" && (string)f23.Content == "❎" && (string)f32.Content == "❎" && (string)f41.Content == "❎")
+            {
+                MessageBox.Show("Вы победили!");
+                d = true;
+                reload();
+            }
+            //
+            if (((string)f00.Content == "⭕" && (string)f01.Content == "⭕" && (string)f02.Content == "⭕" && (string)f03.Content == "⭕") || ((string)f01.Content == "⭕" && (string)f02.Content == "⭕" && (string)f03.Content == "⭕" && (string)f04.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f10.Content == "⭕" && (string)f11.Content == "⭕" && (string)f12.Content == "⭕" && (string)f13.Content == "⭕") || ((string)f11.Content == "⭕" && (string)f12.Content == "⭕" && (string)f13.Content == "⭕" && (string)f14.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f20.Content == "⭕" && (string)f21.Content == "⭕" && (string)f22.Content == "⭕" && (string)f23.Content == "⭕") || ((string)f21.Content == "⭕" && (string)f22.Content == "⭕" && (string)f23.Content == "⭕" && (string)f24.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f30.Content == "⭕" && (string)f31.Content == "⭕" && (string)f32.Content == "⭕" && (string)f33.Content == "⭕") || ((string)f31.Content == "⭕" && (string)f32.Content == "⭕" && (string)f33.Content == "⭕" && (string)f34.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f40.Content == "⭕" && (string)f41.Content == "⭕" && (string)f42.Content == "⭕" && (string)f43.Content == "⭕") || ((string)f41.Content == "⭕" && (string)f42.Content == "⭕" && (string)f43.Content == "⭕" && (string)f44.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f00.Content == "⭕" && (string)f10.Content == "⭕" && (string)f20.Content == "⭕" && (string)f30.Content == "⭕") || ((string)f10.Content == "⭕" && (string)f20.Content == "⭕" && (string)f30.Content == "⭕" && (string)f40.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f01.Content == "⭕" && (string)f11.Content == "⭕" && (string)f21.Content == "⭕" && (string)f31.Content == "⭕") || ((string)f11.Content == "⭕" && (string)f21.Content == "⭕" && (string)f31.Content == "⭕" && (string)f41.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f02.Content == "⭕" && (string)f12.Content == "⭕" && (string)f22.Content == "⭕" && (string)f32.Content == "⭕") || ((string)f12.Content == "⭕" && (string)f22.Content == "⭕" && (string)f32.Content == "⭕" && (string)f42.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f03.Content == "⭕" && (string)f13.Content == "⭕" && (string)f23.Content == "⭕" && (string)f33.Content == "⭕") || ((string)f13.Content == "⭕" && (string)f23.Content == "⭕" && (string)f33.Content == "⭕" && (string)f43.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f04.Content == "⭕" && (string)f14.Content == "⭕" && (string)f24.Content == "⭕" && (string)f34.Content == "⭕") || ((string)f14.Content == "⭕" && (string)f24.Content == "⭕" && (string)f34.Content == "⭕" && (string)f44.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f00.Content == "⭕" && (string)f11.Content == "⭕" && (string)f22.Content == "⭕" && (string)f33.Content == "⭕") || ((string)f11.Content == "⭕" && (string)f22.Content == "⭕" && (string)f33.Content == "⭕" && (string)f44.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if (((string)f40.Content == "⭕" && (string)f31.Content == "⭕" && (string)f22.Content == "⭕" && (string)f13.Content == "⭕") || ((string)f31.Content == "⭕" && (string)f22.Content == "⭕" && (string)f13.Content == "⭕" && (string)f04.Content == "⭕"))
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if ((string)f01.Content == "⭕" && (string)f12.Content == "⭕" && (string)f23.Content == "⭕" && (string)f34.Content == "⭕")
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if ((string)f10.Content == "⭕" && (string)f21.Content == "⭕" && (string)f32.Content == "⭕" && (string)f43.Content == "⭕")
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if ((string)f03.Content == "⭕" && (string)f12.Content == "⭕" && (string)f21.Content == "⭕" && (string)f30.Content == "⭕")
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+            if ((string)f14.Content == "⭕" && (string)f23.Content == "⭕" && (string)f32.Content == "⭕" && (string)f41.Content == "⭕")
+            {
+                MessageBox.Show("Вы проиграли!");
+                reload();
+            }
+        }
+        private void Field_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)e.Source;
+            if((string)b.Content == "▢")
+            {
+                b.Content = "❎";
+            }
+            b.IsEnabled = false;
+            win_check();
+            comp_move();
+            if(d==false)
+            {
+                win_check();
+            }
+        }
+    }
+}
---
+++ lab01/lab01/Win4.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab01
+{
+    /// <summary>
+    /// Логика взаимодействия для Win4.xaml
+    /// </summary>
+    public partial class Win4 : Window
+    {
+        public Win4()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)e.Source;
+            if((string)Field.Content=="0")
+            {
+                Field.Content = "";
+            }
+            Field.Content += (string)b.Content;
+        }
+
+        private void b_dot_Click(object sender, RoutedEventArgs e)
+        {
+            string temp = (string)Field.Content;
+            if (!temp.Contains(","))
+            {
+                Field.Content += ",";
+            }
+        }
+
+        private void b_inverse_Click(object sender, RoutedEventArgs e)
+        {
+            double temp = Convert.ToDouble(Field.Content);
+            temp *= -1;
+            Field.Content =Convert.ToString(temp);
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)e.Source;
+            res.Content += (string)Field.Content+(string)b.Content;
+            Field.Content = "0";
+        }
+
+        private void b_del_Click(object sender, RoutedEventArgs e)
+        {
+            string temp = (string)Field.Content;
+            if (temp.Length != 0)
+            {
+                temp = temp.Remove(temp.Length - 1);
+                Field.Content = temp;
+            }
+        }
+
+        private void b_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = "0";
+            res.Content = "";
+        }
+
+        private void b_Finish_Click(object sender, RoutedEventArgs e)
+        {
+            double result;
+            res.Content += (string)Field.Content;
+            string exp = (string)res.Content;
+            char[] separator = { '+', '-', '*', '/' };
+            string[] temp = exp.Split(separator);
+            string act = "";
+            for(int i=0;i<exp.Length;i++)
+            {
+                if(!char.IsDigit(exp[i]))
+                {
+                    act += exp[i];
+                }
+            }
+            result = Convert.ToDouble(temp[0]);
+            for(int i=0;i<act.Length;i++)
+            {
+                if(act[i]=='+')
+                {
+                    result += Convert.ToDouble(temp[i + 1]);
+                }
+                if(act[i]=='-')
+                {
+                    result -= Convert.ToDouble(temp[i + 1]);
+                }
+                if(act[i]=='/')
+                {
+                    result /= Convert.ToDouble(temp[i+1]);
+                }
+                if(act[i]=='*')
+                {
+                    result *= Convert.ToDouble(temp[i + 1]);
+                }
+            }
+            res.Content += "=";
+            Field.Content = Convert.ToString(result);
+        }
+
+        private void Back4_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            Hide();
+            mw.Show();
+        }
+    }
+}
