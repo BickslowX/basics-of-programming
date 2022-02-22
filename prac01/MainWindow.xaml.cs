721c8b7d0ebc32b330d593cdc1d4d262da511f69
Leonid <90851358+BickslowX@users.noreply.github.com>
2022-02-22 14:19:46 +0200

    Prac01 Done
    Prac01 Done

---
+++ prac01/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prac01
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudyModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Study study = new Study();
+            Hide();
+            study.Show();
+        }
+
+        private void ProtectionModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Authentication auth = new Authentication();
+            Hide();
+            auth.Show();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
